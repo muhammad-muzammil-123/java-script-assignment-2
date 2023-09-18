b8fcd263fd320603da52bcd1b13be5548a9df5bf
Muhammad Muzammil <126906348+muhammad-muzammil-123@users.noreply.github.com>
2023-09-18 09:26:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,286 @@
+// #*#*#*#*#*#*#**#*#*#*#*#*#**#*#*# CHAPTER (6)TO (9)  #*#*#*#*#*##*#*#*#*#**#*#*##**
+// #################### QUESTION NO: 1 #################
+// var a = 10;
+// a++;
+// document.write("the value of a is "+ a + "<br>");
+// ++a;
+// document.write("the value of a is "+ a + "<br>");
+// --a
+// document.write("the value of a is "+ a + "<br>");
+// a--;
+// document.write("the value of a is "+ a + "<br>");
+
+// ################## QUESTION NO:2 ####################
+// var a = 2;
+// var b = 1;
+// --a
+// --a - b--
+// --a - --b + ++b;
+
+// document.write("the result is "+result)
+
+// ################## QUESTIION NO:3 ###############
+//  var name = prompt("Enter your name");
+//  alert("hello "+ name);
+
+// // ################### QUESTION NO: 4 ##############
+// var multiplication = prompt("Enter a number",5);
+// alert(multiplication*2);
+
+// ############### QUESTION NO: 5####################
+// document.write("<h3>Subject    total marks  obtained marks percentage  </h3>");
+
+// var eng = "English";
+// var isl = "Islamiat";
+// var math = "mathematics";
+// var engTotalNum = 100;
+// var islTotalNum = 100;
+// var mathTotalNum = 100;
+//  var engObtained = 68;
+// var islObtained = 75;
+// var mathObtained = 45;
+
+
+// document.write( eng + "   "+ "--->"  + engTotalNum +"  "+" --------------> " + " " + engObtained + "  "+"-------------->"+engObtained / engTotalNum * 100 + "%" +"<br>");
+// document.write( isl + "   "+ "--->"   + islTotalNum + " " +" ------------->" + " " + islObtained+"----------------->" +islObtained / islTotalNum * 100 +" %" +"<br>");
+// document.write( math + "   "+ "->"   + mathTotalNum + " " + " --------------->" + mathObtained + " "+ " --------------->" + mathObtained / mathTotalNum *100 + "%" +" <br>");
+
+// #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##* CHAPTER (9) TO (11) #*#*#*#*#*#*#*#*#*#*
+// ################ QUESTION NO:1 ################
+//  var city = prompt("Enter your city");
+//  if(city === "karachi"){
+//     alert("Welcome to the city of lights")
+//  }
+
+// ######################### QUESTION NO:2 ##################
+//  var gender = prompt("Enter your gender");
+//  if (gender === "male"){
+//     alert("good morning sir");
+//  }if(gender === "female"){
+//     alert("good morning ma'aam")
+//  }
+
+// ###################### QUESTION NO: 3 ####################### 
+// var trafficSignal = prompt("Enter your color");
+// if(trafficSignal === "red"){
+// alert("must stop");
+// }if(trafficSignal === "yellow"){
+// alert("ready to move")
+// }if(trafficSignal === "green"){
+//     alert("move now")
+// }
+
+// ################# QUESTION NO: 4 ################
+ 
+//  var fuelLiters = prompt("check the fuel");
+// if(fuelLiters < 3){
+//     alert("please fill the fuel");
+// }else{
+//     alert("fuel he")
+// }
+
+// ############### QUESTION NO: 5 ####################
+
+//  var a =  12;
+// if(++a == 13){
+//     alert("this condition is true")
+// }
+// var b  = 13;
+//  if( b++ == 14){
+//     alert("this condition is true")
+//  }else{
+//     alert("this condition is false")
+//  }
+
+// var c = 12;
+// if( c++ === 13){
+//     alert("condion one is true")
+// }if(c === 13){
+// alert("condition two is true")
+// }if(++c < 14){
+// alert("condition three is true")
+// }if(c === 14){
+// alert("condition four is true")
+// }
+// var materialCost = 20000;
+// var labourCost = 2000;
+// var totalCost = materialCost + labourCost;
+// if(totalCost === labourCost + materialCost){
+//     alert("the cost is equal..........")
+// }
+
+
+// ############ QUESTION NO: 6 #######################
+// var result = +prompt("enter your subject number");
+
+// if(result >= 80){
+//  alert("your grade is A-One")
+// }if(result >= 70){
+//     alert("your grade is A")
+// }if(result >= 60){
+//     alert("your grade is B")
+// }if(result >= 50){
+//     alert("your grade is C")
+// }if(result < 50){
+//     alert("you are failed")
+// }
+
+// ################### QUESTION NO : 7 ###############
+// var checkNum = +prompt("enter a number");
+// if(checkNum % 2 == 0 ){
+//     alert("this is an even number")
+// }else{
+//     alert("this is an odd number")
+// }
+
+// ########################## QUESTION NO:8 ######################
+// var firstNum = +prompt("enter a first number")
+// var operator = +prompt("Enter a operator for calculation ")
+// var secondNum = +prompt("Enter a second number ");
+// if(operator === '+') {
+//    var result =  firstNum + secondNum;
+//    alert(result) 
+// }if(operator === '-'){
+//     var result =  firstNum - secondNum
+//     alert(result)
+// }if(operator === '*'){
+//     var result= firstNum * secondNum;
+//     alert(result)
+// }if(operator === '/'){
+//  var result =  firstNum / secondNum;
+//  alert(result)
+// }
+
+// ################## QUESTION NO:9 ####################3
+// var integer1 = prompt("enter a first number");
+// var integer2 =prompt("enter a second number");
+// if(integer1 > integer2){
+//     alert(integer1 + "is greater than " + integer2 )
+// }else if( integer1 < integer2){
+//     alert(integer1 + "is less than "+ integer2)
+// }else{
+//     alert("both integer are equal")
+// }
+
+// #*#*#*#*#*#*#**#**#*#*#*#*#*#*#**# CHAPTER (11) TO (13) #*##*#*#**#*#*#*#*#**#
+//  ################## QUESTION NO:1####################
+// var numberOrString = +prompt("enter a value ");
+// if(numberOrString === Number){
+//     console.log("this is a string ");
+
+// }else if(numberOrString === String){
+// console.log("this is a string ");
+
+// }else{
+//     console.log("this is not string or number")
+// }
+
+// ########################## QUESTION NO:2 ################3
+//  var integer1 = +prompt("Enter a number");
+//  var integer2 = +prompt("Enter a second number"); 
+//  if( integer1 >  integer2){
+//     console.log(integer1 + " is greater than " + integer2);
+
+//  }else if(integer1  < integer2){
+// console.log(integer1 + " is less than " + integer2);
+//  }else{
+//     console.log("both integer are equal");
+//  }
+
+// ################## QUESTION NO:3 ##################
+// var vowels = prompt("Enter a one charactor");
+// if(vowels ===  "a"){
+//     console.log("this is a vowels "+ vowels)
+// }else if(vowels === "e"){
+//     console.log("this is a vowels "+ vowels)
+// }else if(vowels === "i"){
+//     console.log("this is a vowels "+ vowels)
+// }else if(vowels === "o"){
+//     console.log("this is a vowels "+ vowels)
+// }else if(vowels === "u"){
+//     console.log("this is a vowels "+ vowels)
+// }else{
+//     console.log("this is not vowels "+ vowels)
+// }
+
+// ################### QUESTION  NO:4 ################
+
+// var num = +prompt("Enter a number");
+// if(num > 0){
+//     console.log("the number is positive ")
+// }else if(num < 0){
+//     console.log("the number is negative");
+
+// }else{
+//     console.log("the number is zero");
+// }
+
+// ################# QUESTION NO: 5 ######################3
+// var correctPass = "03032375160";
+// var userPass = prompt("Enter a correct password");
+// if( userPass === correctPass){
+//     alert("correct")
+// }else{
+//     alert("please enter a correct password");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
